55207e1b67a2dce31d99e981f3a21683f7697712
HAL <43222922+halBRY@users.noreply.github.com>
2024-10-07 11:59:05 -0500

    Create send_gripper.py

---
+++ Omniverse/end_effectors/send_gripper.py
@@ -0,0 +1,79 @@
+import logging
+import math
+from pprint import pprint
+
+import numpy as np
+import omni
+import omni.kit.pipapi
+import omni.physx.scripts.utils as pxutils
+from omni.isaac.core.articulations.articulation import Articulation
+from omni.isaac.core.utils.types import ArticulationAction
+from omni.kit.scripting import BehaviorScript
+from pxr import (Gf, PhysxSchema, Sdf, Tf, Usd, UsdGeom, UsdLux, UsdPhysics,
+                 UsdShade, UsdUtils)
+
+try:
+    import zmq
+except ModuleNotFoundError:
+    omni.kit.pipapi.install("zmq")
+    import zmq
+
+logger = logging.getLogger(__name__)
+commands = []
+
+# This script should be placed on the robot xform in the OVIS Scene.
+# It gets the joints and sends to Unity
+
+class RobotControl(BehaviorScript):
+    def on_init(self):
+        logger.info(f"{__class__.__name__}.on_init()->{self.prim_path}")
+
+        self.robot = None
+
+        self.context = zmq.Context()
+
+        self.sock = self.context.socket(zmq.PUB)
+        self.sock.bind("tcp://*:12348") #FOR GRIPPER
+
+        self.had_first_update = False
+
+    def on_destroy(self):
+        logger.info(f"{__class__.__name__}.on_destroy()->{self.prim_path}")
+
+        self.context.destroy()
+
+    def on_play(self):
+        logger.info(f"{__class__.__name__}.on_play()->{self.prim_path}")
+
+        #logger.warn("UR Playing")
+
+        self.had_first_update = False
+
+    def on_pause(self):
+        logger.info(f"{__class__.__name__}.on_pause()->{self.prim_path}")
+
+    def on_stop(self):
+        logger.info(f"{__class__.__name__}.on_stop()->{self.prim_path}")
+
+        self.had_first_update = False
+
+    def on_first_update(self, current_time: float, delta_time: float):
+        self.had_first_update = True
+
+        self.robot = Articulation(str(self.prim_path))
+        self.robot.initialize()
+
+    def on_update(self, current_time: float, delta_time: float):
+        if not self.had_first_update:
+            self.on_first_update(current_time, delta_time)
+        
+        # Get gripper attribute state
+        switch = self.stage.GetPrimAtPath(str(self.prim_path)+ '/Switch')
+        switch_state = switch.GetAttribute("JWM:Switch_state").Get()
+    
+
+        # Send joint positions as a single string
+        try:
+            self.sock.send_string(str(switch_state))
+        except zmq.ZMQError as exc:
+            logger.warn(exc)
